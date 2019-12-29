39201ca8f8dd6f26b3f8312f795ae41f55003e0c
Ghassen Khalil Ati <ghassen1khalil@gmail.com>
2019-12-29 23:10:31 +0100

    add infoResource controller

---
+++ api-login/src/main/java/fr/developer/api/login/controller/InfoResource.java
@@ -0,0 +1,19 @@
+package fr.developer.api.login.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/info")
+public class InfoResource {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/appname")
+    public String getAppName(){
+        return appName;
+    }
+}
